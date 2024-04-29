d227f11e4cd448c081713c92313e124a6dc082ac
viveksinghtt <vivek.singh@tradingtechnologies.com>
2024-04-29 10:45:27 +0530

    Update build-role.tf

--- build-role.tf
+++ build-role.tf
@@ -46,7 +46,18 @@ data "aws_iam_policy_document" "build" {
       "*"
     ]
   }
-
+  statement {
+    sid = "AllowKMS"
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
   statement {
     effect = "Allow"
     actions = [
